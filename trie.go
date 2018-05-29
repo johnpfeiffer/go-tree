f8aa2195a8fa26c7cd69025678dba0bed10b85b8
john pfeiffer <foupfeiffer@gmail.com>
2018-05-28 21:48:37 -0700

    refactor to use a map instead of an array and TDD implement Insert, Find, and getWords()

--- trie.go
+++ trie.go
@@ -2,37 +2,80 @@ package gotree
 
 // TrieNode is a node in a prefix tree https://en.wikipedia.org/wiki/Trie
 type TrieNode struct {
-	key      rune
-	children []*TrieNode
+	Key      rune
+	Children map[rune]*TrieNode
 }
 
 // TODO: is a string more readable and just as efficient as a rune?
 
-/*
 // TrieInsert adds the string into the tree (adding nodes as necessary)
 func TrieInsert(root *TrieNode, s string) {
 	current := root
-	index := 0
-
-	for {
-		if index == len(s)-1 {
-			break
+	for index := 0; index < len(s); index++ {
+		if current.Children == nil {
+			current.Children = make(map[rune]*TrieNode)
 		}
-		next := nextNode(current.children, rune(s[index]))
-		if next == nil {
-			n := &TrieNode{key: rune(s[index])}
-			current.children = append(current.children, n)
+		next, ok := current.Children[rune(s[index])]
+		if !ok {
+			next = &TrieNode{Key: rune(s[index])}
+			current.Children[rune(s[index])] = next
 		}
 		current = next
 	}
 }
 
-*/
+// Find returns if a string is in the Trie
+func (t *TrieNode) Find(s string) bool {
+	current := t
+	index := 0
+	for ; index < len(s); index++ {
+		next, ok := current.Children[rune(s[index])]
+		if !ok {
+			break
+		}
+		current = next
+	}
+	if index == len(s) {
+		return true
+	}
+	return false
+}
 
+// getWords gets all of the terminating strings in the Trie
+func (t *TrieNode) getWords() []string {
+	// leaf node is the base case
+	if len(t.Children) == 0 {
+		// do not return the root key that is the rune zero value
+		if t.Key != 0 {
+			return []string{string(t.Key)}
+		}
+		return []string{}
+	}
+
+	// TODO: refactor to enumerate alphabetically
+	result := []string{}
+	for _, r := range t.Children {
+		result = append(result, r.getWords()...)
+	}
+
+	// do not return the root key that is the rune zero value
+	if t.Key == 0 {
+		return result
+	}
+
+	final := []string{}
+	for _, s := range result {
+		final = append(final, string(t.Key)+s)
+	}
+	return final
+}
+
+// TODO: remove unused?
 // nextNode discovers if a new node is necessary in the tree or returns a pointer to the next node
 func nextNode(children []*TrieNode, target rune) *TrieNode {
+	// TODO: this is slow but allows enumeration alphabetically
 	for _, c := range children {
-		if target == c.key {
+		if target == c.Key {
 			return c
 		}
 	}
--- trie_test.go
+++ trie_test.go
@@ -5,7 +5,36 @@ import (
 	"testing"
 )
 
-func TestNextNode(t *testing.T) {
+func TestTrieInsertBasic(t *testing.T) {
+	root := &TrieNode{} // root has no key associated with it, only children
+	TrieInsert(root, "hi")
+	assertTrieKey(t, root.Key, 0)
+
+	assertTrieKey(t, root.Children['h'].Key, 'h')
+	assertTrieKey(t, root.Children['h'].Children['i'].Key, 'i')
+
+	assertBoolean(t, true, root.Find("hi"))
+	assertBoolean(t, false, root.Find("high"))
+
+	contents := root.getWords()
+	assertNumber(t, "number of leaf nodes in the Trie", 1, len(contents))
+	assertString(t, "Trie contents", "hi", contents[0])
+
+	TrieInsert(root, "hello")
+
+	assertBoolean(t, true, root.Find("hi"))
+	assertBoolean(t, true, root.Find("hello"))
+	assertBoolean(t, true, root.Find("he"))
+	assertBoolean(t, true, root.Find("hell"))
+	assertBoolean(t, false, root.Find("high"))
+	assertBoolean(t, false, root.Find("hella"))
+	contents = root.getWords()
+	assertNumber(t, "number of leaf nodes in the Trie", 2, len(contents))
+
+	assertString(t, "Trie contents", "hi", contents[0])
+	assertString(t, "Trie contents", "hello", contents[1])
+}
+func TestTrieNextNode(t *testing.T) {
 	var testCases = []struct {
 		children []*TrieNode
 		target   rune
@@ -13,10 +42,10 @@ func TestNextNode(t *testing.T) {
 	}{
 		{children: nil, target: 'a', expected: 0},
 		{children: []*TrieNode{}, target: 'a', expected: 0},
-		{children: []*TrieNode{&TrieNode{key: 'a'}}, target: 'a', expected: 'a'},
-		{children: []*TrieNode{&TrieNode{key: 'a'}, &TrieNode{key: 'b'}}, target: 'a', expected: 'a'},
-		{children: []*TrieNode{&TrieNode{key: 'a'}, &TrieNode{key: 'b'}}, target: 'b', expected: 'b'},
-		{children: []*TrieNode{&TrieNode{key: 'a'}, &TrieNode{key: 'b'}}, target: 'c', expected: 0},
+		{children: []*TrieNode{&TrieNode{Key: 'a'}}, target: 'a', expected: 'a'},
+		{children: []*TrieNode{&TrieNode{Key: 'a'}, &TrieNode{Key: 'b'}}, target: 'a', expected: 'a'},
+		{children: []*TrieNode{&TrieNode{Key: 'a'}, &TrieNode{Key: 'b'}}, target: 'b', expected: 'b'},
+		{children: []*TrieNode{&TrieNode{Key: 'a'}, &TrieNode{Key: 'b'}}, target: 'c', expected: 0},
 		{children: generateTrieChildren(t, []rune{'a', 'b', 'c', 'd', 'e', 'A', 'B', 'Z'}), target: 'A', expected: 'A'},
 		{children: generateTrieChildren(t, []rune{'a', 'b', 'c', 'd', 'e', 'A', 'B', 'Z'}), target: 'z', expected: 0},
 	}
@@ -25,21 +54,41 @@ func TestNextNode(t *testing.T) {
 			var result rune
 			n := nextNode(tc.children, tc.target)
 			if n != nil {
-				result = n.key
+				result = n.Key
 			}
-			if tc.expected != result {
-				t.Errorf("Expected %v but received %v", tc.expected, result)
-			}
+			assertTrieKey(t, tc.expected, result)
 		})
 	}
 }
 
 // helpers
+
 func generateTrieChildren(t *testing.T, a []rune) []*TrieNode {
 	t.Helper()
 	result := []*TrieNode{}
 	for _, r := range a {
-		result = append(result, &TrieNode{key: r})
+		result = append(result, &TrieNode{Key: r})
 	}
 	return result
+}
+
+func assertTrieKey(t *testing.T, expected, actual rune) {
+	t.Helper()
+	if expected != actual {
+		t.Errorf("Expected %#v but received %#v", expected, actual)
+	}
+}
+
+func assertBoolean(t *testing.T, expected, actual bool) {
+	t.Helper()
+	if expected != actual {
+		t.Errorf("Expected %v but received %v", expected, actual)
+	}
+}
+
+func assertString(t *testing.T, hint, expected, actual string) {
+	t.Helper()
+	if expected != actual {
+		t.Errorf("%s Expected %#v but received %#v", hint, expected, actual)
+	}
 }
